ba0a32b43e127ef6a24e9f9eae7d822853c33f86
DatVuTB <datvu080504@gmail.com>
2026-07-01 23:29:34 +0700

    Cấu hình DB

---
+++ AppTuyenDung/src/main/java/config/DatabaseConnection.java
@@ -0,0 +1,12 @@
+package org.example.config;
+
+public class DatabaseConfig {
+    public static final String Url = "jdbc:postgresql://103.216.117.40:15432/webtuyendung";
+    public static final String User = "webtuyendung";
+    public static final String Password = "123456";
+    public static final String DB = "webtuyendung";
+
+    public static  DatabaseConfig getConnection() throws SQLException {
+        return DriverManager.getConnection(Url,User,Password);
+    }
+}

42335fe6eaa0acbe549e8d72cee2b98a8c8f1f00
DatVuTB <datvu080504@gmail.com>
2026-07-04 09:21:48 +0700

    Cấu hình minIo

--- AppTuyenDung/src/main/java/config/MinIOConfig.java
+++ AppTuyenDung/src/main/java/config/MinIOConfig.java
@@ -1,5 +1,12 @@
 package config;
 
+import io.minio.MinioClient;
+
 // thư mục other-project
 public class MinIOConfig {
+        private static final MinioClient CLIENT = MinioClient.builder().endpoint("http://s3.103.216.117.40.nip.io").credentials("scontract","scontract@2025").build();
+
+        public  static MinioClient getClient(){
+                return CLIENT;
+        }
 }
